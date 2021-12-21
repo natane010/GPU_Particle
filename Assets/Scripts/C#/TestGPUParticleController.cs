96f3476eeade68aad4137c5ba8fdce937984190d
natane010 <mimikaumi@hotmail.com>
2021-12-22 00:30:51 +0900

    テストシーンの追加＋別プロジェクトからパーティクルシステムの移送

---
+++ Assets/Scripts/C#/TestGPUParticleController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGPUParticleController : MonoBehaviour
+{
+    [SerializeField] private GPUParticleRootSystem _particleSystem = null;
+    [SerializeField] private GPUParticleTargetGroups[] _groups = null;
+    [SerializeField] private float _radius = 3f;
+
+    private GPUParticleTargetGroups CurrentGroup => _groups[_index];
+
+    private int _index = 0;
+
+    private InitData[] _initData = null;
+
+    
+    private IEnumerator Start()
+    {
+        Initialize();
+
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (var g in _groups)
+        {
+            g.Initialize(_particleSystem);
+        }
+
+        _particleSystem.SetGroup(_groups[0]);
+        _particleSystem.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Next();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Gravity();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Explosion();
+        }
+    }
+    private void Initialize()
+    {
+        _initData = new InitData[_particleSystem.ParticleCount];
+
+        for (int i = 0; i < _initData.Length; i++)
+        {
+            _initData[i] = new InitData();
+        }
+    }
+
+    private void Next()
+    {
+        _index = (_index + 1) % _groups.Length;
+        _particleSystem.ChangeUpdateMethod(UpdateMethodType.Target);
+        _particleSystem.SetGroup(CurrentGroup);
+    }
+
+    private void Gravity()
+    {
+        _particleSystem.SetOrigin(Vector3.one);
+        _particleSystem.ChangeUpdateMethodWithClear(UpdateMethodType.Gravity);
+    }
+
+    private void Explosion()
+    {
+        for (int i = 0; i < _initData.Length; i++)
+        {
+            _initData[i].isActive = 1;
+            _initData[i].scale = 2.0f;
+            _initData[i].horizontal = Random.onUnitSphere;
+            Vector3 v = Vector3.forward;
+            float w = Random.Range(1f, 3f);
+
+            float d = Vector3.Dot(v, _initData[i].horizontal);
+
+            if (d < 0)
+            {
+                v = (v - _initData[i].horizontal);
+            }
+            else
+            {
+                v = (v - _initData[i].horizontal);
+            }
+
+            _initData[i].velocity = new Vector4(v.x, v.y, v.z, w);
+        }
+
+        _particleSystem.SetOrigin(Vector3.one);
+        _particleSystem.UpdateInitData(_initData);
+        _particleSystem.ChangeUpdateMethodWithClear(UpdateMethodType.Explode);
+    }
+}
+
